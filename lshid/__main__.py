ecaa939167053d3e9a1755f07194c5281b0b2353
Filipe Laíns <lains@archlinux.org>
2020-09-09 19:48:36 +0100

    lshid: put helper functions into a class and get rid of global variable
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- lshid/__main__.py
+++ lshid/__main__.py
@@ -14,53 +14,51 @@ import parse
 import lshid
 
 
-devices: Dict[str, ioctl.hidraw.Hidraw] = {}
+class DeviceHolder():
+    def __init__(self) -> None:
+        self.devices: Dict[str, ioctl.hidraw.Hidraw] = {}
 
+    def iterate_devices(self) -> Iterator[str]:
+        for fname in os.listdir('/dev/'):
+            if fname.startswith('hidraw'):
+                yield fname
 
-def _get_devices() -> Iterator[str]:
-    for fname in os.listdir('/dev/'):
-        if fname.startswith('hidraw'):
-            yield fname
+    def add_device(self, path: str) -> None:
+        if os.path.exists(path):
+            try:
+                self.devices[path] = ioctl.hidraw.Hidraw(path)
+            except IOError as e:
+                print(f"can't open '{path}': {str(e)}", file=sys.stderr)
 
+    def filter_devices(self, hid_info: str) -> None:
+        bus_pattern = parse.compile('{bus:d}:{vid:x}:{pid:x}')
+        sep_pattern = parse.compile(':{vid:x}:{pid:x}')
+        dev_pattern = parse.compile('{vid:x}:{pid:x}')
 
-def _add_device(path: str) -> None:
-    if os.path.exists(path):
-        try:
-            devices[path] = ioctl.hidraw.Hidraw(path)
-        except IOError as e:
-            print(f"can't open '{path}': {str(e)}", file=sys.stderr)
+        for key, hidraw in self.devices.copy().items():
+            bus, vid, pid = hidraw.info
+            for pattern in [bus_pattern, sep_pattern, dev_pattern]:
+                keys = pattern.parse(hid_info)
+                if keys:
+                    if (
+                        'bus' in keys and bus != keys['bus'] or
+                        vid != keys['vid'] or
+                        pid != keys['pid']
+                    ):
+                        del self.devices[key]
+                    break
 
-
-def _filter_devices(hid_info: str) -> None:
-    bus_pattern = parse.compile('{bus:d}:{vid:x}:{pid:x}')
-    sep_pattern = parse.compile(':{vid:x}:{pid:x}')
-    dev_pattern = parse.compile('{vid:x}:{pid:x}')
-
-    for key, hidraw in devices.copy().items():
-        bus, vid, pid = hidraw.info
-        for pattern in [bus_pattern, sep_pattern, dev_pattern]:
-            keys = pattern.parse(hid_info)
-            if keys:
-                if (
-                    'bus' in keys and bus != keys['bus'] or
-                    vid != keys['vid'] or
-                    pid != keys['pid']
-                ):
-                    del devices[key]
-                break
-
-
-def _print_devices(verbose: bool = False) -> None:
-    first = True
-    for key, hidraw in devices.items():
-        bus, vid, pid = hidraw.info
-        if verbose and not first:
-            print()
-        print(f'Device {key}: ID {vid:04x}:{pid:04x} {hidraw.name}')
-        if verbose:
-            print('Report Descriptor:')
-            print(hidraw.report_descriptor)
-        first = False
+    def print_devices(self, verbose: bool = False) -> None:
+        first = True
+        for key, hidraw in self.devices.items():
+            bus, vid, pid = hidraw.info
+            if verbose and not first:
+                first = False
+                print()
+            print(f'Device {key}: ID {vid:04x}:{pid:04x} {hidraw.name}')
+            if verbose:
+                print('Report Descriptor:')
+                print(hidraw.report_descriptor)
 
 
 def main_parser() -> argparse.ArgumentParser:
@@ -104,21 +102,23 @@ def main(cli_args: List[str], prog: Optional[str] = None) -> None:
         print(f'lshid {lshid.__version__}')
         return
 
+    main = DeviceHolder()
+
     if args.devnum is not None:
-        _add_device(f'/dev/hidraw{args.devnum}')
+        main.add_device(f'/dev/hidraw{args.devnum}')
     elif args.file is not None:
-        _add_device(args.file)
+        main.add_device(args.file)
     else:
-        for device in _get_devices():
-            _add_device(f'/dev/{device}')
+        for device in main.get_devices():
+            main.add_device(f'/dev/{device}')
 
         if args.hid_info:
-            _filter_devices(args.hid_info)
+            main.filter_devices(args.hid_info)
 
-    if not devices:
+    if not main.devices:
         exit(1)
 
-    _print_devices(args.verbose)
+    main.print_devices(args.verbose)
 
 
 def entrypoint() -> None:
